2a3935b72a0a80a3d5f2a6f5a3bfb52cfa5a460c
kcrmin <ryanmin.dev@gmail.com>
2023-07-06 22:18:00 +0800

    clean

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
-from fastapi.params import Body
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from typing import Optional
+
 
 app = FastAPI()
 
@@ -12,8 +12,7 @@ class Post(BaseModel):
     content: str
     published: bool = True
     rating: Optional[int] = None
-
-
+    
 @app.get("/")
 def root():
     return {"message": "Hello World"}
@@ -22,6 +21,14 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    # if not available
+    if id == 2:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"FUCK")
+    return {"post_detail": f"Here is the post {id}"}
 
 
 @app.post("/createposts")
